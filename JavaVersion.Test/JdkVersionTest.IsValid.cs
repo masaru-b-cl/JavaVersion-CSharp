d61c7c04ad5e14e5a391b5d436fbb5324376b9c6
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2013-05-23 13:10:47 +0900

    split and group test

---
+++ JavaVersion.Test/JdkVersionTest.IsValid.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+
+namespace JavaVersion.Test
+{
+  public partial class JdkVersionTest
+  {
+    [TestCategory("IsValid")]
+    [TestMethod]
+    public void 妥当なバージョン番号ならtrueを返す()
+    {
+      JdkVersion.IsValid("JDK10u40").Is(true);
+    }
+
+    [TestCategory("IsValid")]
+    [TestMethod]
+    public void JDKで始まらないならfalseを返す()
+    {
+      JdkVersion.IsValid("hoge").Is(false);
+    }
+
+    [TestCategory("IsValid")]
+    [TestMethod]
+    public void familyVersionが数値でないならfalseを返す()
+    {
+      JdkVersion.IsValid("JDKxu40").Is(false);
+    }
+
+    [TestCategory("IsValid")]
+    [TestMethod]
+    public void familyVersionとupdateVersionの区切りがuでないならfalseを返す()
+    {
+      JdkVersion.IsValid("JDK7x40").Is(false);
+    }
+
+    [TestCategory("IsValid")]
+    [TestMethod]
+    public void updateVersionが数値でないならfalseを返す()
+    {
+      JdkVersion.IsValid("JDK7u9x").Is(false);
+    }
+
+    [TestCategory("IsValid")]
+    [TestMethod]
+    public void familyVersionが0ならfalseを返す()
+    {
+      JdkVersion.IsValid("JDK00u40").Is(false);
+    }
+
+    [TestCategory("IsValid")]
+    [TestMethod]
+    public void updateVersionが0ならfalseを返す()
+    {
+      JdkVersion.IsValid("JDK7u00").Is(false);
+    }
+  }
+}
---
+++ JavaVersion.Test/JdkVersionTest.Learning.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+
+namespace JavaVersion.Test
+{
+  public partial class JdkVersionTest
+  {
+    [TestCategory("Learning")]
+    [TestMethod]
+    public void 正規表現のグループの実装確認()
+    {
+      var regex = new Regex("a(?<num>[0-9]+?)b");
+      var match = regex.Match("a0b");
+      match.Success.Is(true);
+      match.Groups["num"].Value.Is("0");
+    }
+  }
+}
